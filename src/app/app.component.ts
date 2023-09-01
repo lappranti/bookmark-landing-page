8012b0f9eaca50d6680ccd8e67577fa4719a2b09
lappranti <ndeurys@gmail.com>
2023-09-01 16:51:14 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'bookmark-landing-page';
+export class AppComponent implements OnInit {
+  isMenuVisible: boolean = false;
+
+  ngOnInit(): void {
+    this.handleSelectTab('tab1');
+  }
+
+  handleToggleMenu() {
+    this.isMenuVisible = !this.isMenuVisible;
+  }
+
+  handleSelectTab(currentSelectedId: string) {
+    const nextSelectedTab = document.querySelector('.select');
+    const nextActiveContent = document.querySelector('.active') as HTMLElement;
+    if (nextSelectedTab?.getAttribute('id') != currentSelectedId) {
+      const currentSelectedTab = document.getElementById(currentSelectedId);
+      const currentActiveContent = document.querySelector(
+        '.' + currentSelectedId
+      );
+
+      nextSelectedTab?.classList.remove('select');
+      nextActiveContent?.classList.remove('active');
+
+      currentSelectedTab?.classList.add('select');
+      currentActiveContent?.classList.add('active');
+    }
+  }
+
+  handleToggleQuestio(idQuestion: string) {
+    const question = document.getElementById(idQuestion);
+    question?.classList.toggle('select');
+    question?.nextElementSibling?.classList.toggle('select');
+  }
+
+  showGoToTopButton = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // Afficher ou masquer le bouton en fonction de la position de défilement
+    this.showGoToTopButton = window.scrollY > 100; // à partir de 100 pixels de défilement
+  }
+
+  scrollToTop() {
+    // Faire défiler la page vers le haut de manière fluide
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
